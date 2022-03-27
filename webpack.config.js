b5b9e5e8919840522d765c08be1c03f418e298f8
Piyush Jain <piyushmbm45@gmail.com>
2022-03-27 09:27:37 +0530

    add multiple entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-const path = require('path');
+const path = require("path");
 
-module.exports={
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    }
-}
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+};
